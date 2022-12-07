9477fb6de3da21a384899953c1c01f073df508d7
Abhinav <114936870+abhinav16aero@users.noreply.github.com>
2022-12-07 23:28:58 +0530

    Add files via upload

---
+++ EventsStatic.js
@@ -0,0 +1,51 @@
+import KDSH2020 from "../../../assets/pics/events/KDSH2020.jpg";
+import SummerAI from "../../../assets/pics/events/SummerAIChallenge.jpg";
+import KDSH2021 from "../../../assets/pics/HACKPoster.png";
+import WW21 from "../../../assets/pics/WW21.png";
+import KDSH2022 from "../../../assets/pics/KDSH2022.png";
+
+const events = [
+    {
+        id: 0,
+        title: "Winter Workshop 2021",
+        location: "MS Teams",
+        date: "Starting 16th Dec 2021",
+        image: WW21,
+        resources: "/winter-workshop",
+    },
+    {
+        id: 1,
+        title: "Kharagpur Data Science Hackathon 2021",
+        location: "Dare2Compete",
+        date: "20th Nov 2021 - 7th Dec 2021",
+        link: "http://tinyurl.com/kdshreg",
+        image: KDSH2021,
+        certificates: "/certificate-generation",
+    },
+    {
+        id: 2,
+        title: "Summer AI Challenge 2021",
+        location: "Dare2Compete",
+        date: "1st May 2021 - 8th May 2021",
+        link: "https://dare2compete.com/o/summer-ai-challenge-indian-institute-of-technology-iit-kharagpur-161678?fbclid=IwAR04rjavk6rd5_8wTrSjVj3jJBpFEhnrjXePqDxid_bGRT51ZDcPlID3XXo",
+        image: SummerAI,
+    },
+    {
+        id: 3,
+        title: "Kharagpur Data Science Hackathon, 1st Edition",
+        location: "Dare2Compete",
+        date: "15th March 2021 - 6th April 2021",
+        link: "https://dare2compete.com/o/kharagpur-data-science-hackathon-kharagpur-data-analytics-group-148743?fbclid=IwAR1E1C-_-4gCSdY8FkZ4eaa9wk5FbigCm_JDTDoLn10MNIBikDYso2r1p70",
+        image: KDSH2020,
+    },
+    {
+        id: 4,
+        title: "Kharagpur Data Science Hackathon 2022",
+        location: "Dare2Compete",
+        date: "16th December 2022 - 17th January 2022",
+        link: "https://unstop.com/hackathon/kharagpur-data-science-hackathon-2022-indian-institute-of-technology-iit-kharagpur-542463",
+        image: KDSH2022,
+    }
+];
+
+export default events;
